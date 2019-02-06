0bd65ad9edb716a02fd7ae0b0f5f52f762c2a537
Wong Hoi Sing Edison <hswong3i@gmail.com>
2019-02-06 14:17:47 +0800

    Explicitly set system user UID/GID

--- files/usr/local/bin/docker-entrypoint.sh
+++ files/usr/local/bin/docker-entrypoint.sh
@@ -2,10 +2,5 @@
 
 set -o xtrace
 
-# Ensure required folders exist with correct owner:group
-mkdir -p $JIRA_HOME
-chmod 0755 $JIRA_HOME
-chown -Rf $JIRA_OWNER:$JIRA_GROUP $JIRA_HOME
-
 # Running JIRA in foreground
 exec /etc/init.d/jira start -fg
